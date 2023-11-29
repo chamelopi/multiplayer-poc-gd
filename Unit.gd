3f213732e9a369b9dd2ffe8d9892c9d254a540d0
chamelopi <ben.gorny@web.de>
2023-11-29 22:13:56 +0100

    Make movement sync smoother
    - sync velocity by exporting it
    - run _process() on both the server and the client
    - use 100ms delta replication interval to save a little bandwidth

--- Unit.gd
+++ Unit.gd
@@ -6,24 +6,30 @@ var moving = false
 var direction: Vector2
 var moved = 0.0
 
+# Workaround for https://github.com/godotengine/godot/issues/68516:
+# Due to a bug we can only synchronize exported properties
+@export var my_velocity := Vector3(0., 0., 0.):
+	set(velo):
+		velocity = velo  # to set mine
+		my_velocity = velo  # to set theirs
 
+# This runs on both the server and the client. Because the server owns the units, it has 
+# authority and its position & velocity will be synced to the units.
+# This combination results in smooth movement, which is also correct as per the authority.
 func _process(delta):
-	# Only need to run this on the server, it will get synced to the client automatically
-	if multiplayer and multiplayer.get_unique_id() == 1:
-		if not moving:
-			var rand_angle = randf() * PI * 2
-			direction = Vector2(sin(rand_angle), cos(rand_angle)).normalized()
-			moving = true
-		else:
-			velocity.x = direction.x * SPEED * delta
-			velocity.z = direction.y * SPEED * delta
-			
-			moved += delta
-			
-			if moved >= 2.0:
-				moving = false
-				moved = 0.0
-				velocity = Vector3(0, 0, 0)
+	if not moving:
+		var rand_angle = randf() * PI * 2
+		direction = Vector2(sin(rand_angle), cos(rand_angle)).normalized()
+		moving = true
+	else:
+		velocity.x = direction.x * SPEED * delta
+		velocity.z = direction.y * SPEED * delta
+		
+		moved += delta
+		
+		if moved >= 2.0:
+			moving = false
+			moved = 0.0
+			velocity = Vector3(0, 0, 0)
 				
-		move_and_slide()
-
+	move_and_slide()
